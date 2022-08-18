dd867ed26abfaef6dc1087f7005664462be5caa0
Morgan Bradford <morganjbradford95@gmail.com>
2022-08-18 17:13:03 -0500

    add image upload functionality

--- EmployeeApp/views.py
+++ EmployeeApp/views.py
@@ -6,6 +6,8 @@ from django.http.response import JsonResponse
 from EmployeeApp.models import Departments, Employees
 from EmployeeApp.serializers import DepartmentSerializer, EmployeeSerializer
 
+from django.core.files.storage import default_storage
+
 # Create your views here.
 
 @csrf_exempt
@@ -68,3 +70,10 @@ def employeeApi(request, id=0):
     employee.delete()
     return JsonResponse("Deletion successful!", safe=False)
   return JsonResponse('Unable to delete', safe=False)
+
+@csrf_exempt
+def SaveFile(request):
+  file = request.FILES['uploadedFile']
+  file_name = default_storage.save(file.name,file)
+
+  return JsonResponse(file_name, safe=False)
