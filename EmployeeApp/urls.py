7dae920673785c381b2705d80c3d870f2121072c
Morgan Bradford <morganjbradford95@gmail.com>
2022-08-18 12:49:10 -0500

    WIP: adjust to django updates

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -1,8 +1,8 @@
 from ssl import VERIFY_ALLOW_PROXY_CERTS
-from django.conf.urls import url
+from django.urls import re_path
 from EmployeeApp import views
 
 urlpatterns = [
-  url(r'^department/$',views.departmentApi),
-  url(r'department/([0-9]+)$',views.departmentApi)
+  re_path(r'^department/$',views.departmentApi),
+  re_path(r'department/([0-9]+)$',views.departmentApi)
 ]
