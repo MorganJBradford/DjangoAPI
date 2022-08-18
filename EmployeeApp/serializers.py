aabb36ad1e7d74e2958f78323eb5055bba01501a
Morgan Bradford <morganjbradford95@gmail.com>
2022-08-18 12:13:17 -0500

    define DepartmentSerializer

---
+++ EmployeeApp/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from EmployeeApp.models import Departments, Employees
+
+class DepartmentSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Departments
+    fields = ('DepartmentId',
+              'DepartmentName')
+
